41b10cead5afd3d903b13086d585bf5b2e901b47
Yoseph <yusupjanadil@gmail.com>
2025-10-28 10:05:53 +0100

    start

---
+++ librogeek-backend/src/main/java/com/librogeek/LibrogeekApplication.java
@@ -0,0 +1,13 @@
+package com.librogeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrogeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrogeekApplication.class, args);
+	}
+
+}
---
+++ librogeek-backend/src/test/java/com/librogeek/LibrogeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librogeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrogeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
